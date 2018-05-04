654b9116f099cd3d80e7ed4f489b1aa8200ad16c
Prince Raj <raaz.crzy@gmail.com>
2018-05-04 14:36:33 +0530

    added infite for loop

--- main.go
+++ main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"time"
 )
 
 func main() {
-	fmt.Println("Everything is working fine. Its version 0.0.1")
-	log.Println("Have a nice day, 0.0.1")
+	for {
+		fmt.Println("Everything is working fine. Its version 0.0.5")
+		log.Println("Have a nice day, 0.0.5")
+		time.Sleep(time.Second * 60)
+	}
+	os.Exit(0)
 }
